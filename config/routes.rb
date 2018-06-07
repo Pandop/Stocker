9a6898bfd5597ab268f393c72f2d92bca2f9585d
Pandop <s.kel@student.qut.edu.au>
2018-06-07 14:59:08 +1000

    Added Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'      # Root route. http://localhost:3000/home/index
+
+  get  'home/index'
+  get  'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
